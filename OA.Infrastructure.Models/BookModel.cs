068d11947cdccf957d170e859139ea46c454c91f
Vadim Prokopchuk <Vadim Prokopchuk@VP>
2017-10-28 21:27:58 +0300

    Added models

---
+++ OA.Infrastructure.Models/BookModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OA.Infrastructure.Models
+{
+    public class BookModel
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
