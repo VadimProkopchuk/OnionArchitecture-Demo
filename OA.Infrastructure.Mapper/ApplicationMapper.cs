826e7c2fc3c12f1beed40c986d0c24fd920ca4cc
Vadim Prokopchuk <Vadim Prokopchuk@VP>
2017-10-28 21:28:17 +0300

    Added mapper

---
+++ OA.Infrastructure.Mapper/ApplicationMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using OA.Infrastructure.Mapper.Maps;
+
+namespace OA.Infrastructure.Mapper
+{
+    public static class ApplicationMapper
+    {
+        public static void Configure()
+        {
+            AutoMapper.Mapper.Initialize(InicializeMapper);
+        }
+
+        public static TDestination Map<TSource, TDestination>(this TSource source) =>
+            AutoMapper.Mapper.Map<TSource, TDestination>(source);
+
+        private static void InicializeMapper(IMapperConfigurationExpression config)
+        {
+            BookMap.Configure(config);
+        }
+    }
+}
---
+++ OA.Infrastructure.Mapper/Maps/BookMap.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using OA.Domain.Core;
+using OA.Infrastructure.Models;
+
+namespace OA.Infrastructure.Mapper.Maps
+{
+    internal static class BookMap
+    {
+        public static void Configure(IMapperConfigurationExpression config)
+        {
+            config.CreateMap<Book, BookModel>();
+            config.CreateMap<BookModel, Book>()
+                .ForMember(x => x.Id, opt => opt.Ignore());
+        }
+    }
+}
